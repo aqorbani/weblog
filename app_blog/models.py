3495c90f9003c06e62ed548c734da9e8ad36afed
aqorbani <a.qorbani@gmail.com>
2021-03-07 13:01:05 +0330

    admin-panel & database & article , userprofile , category model created!

---
+++ app_blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from datetime import datetime
+
+
+def validate_file_extention(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extentions = ['.jpg', '.png']
+    if not ext.lower() in valid_extentions:
+        raise ValidationError('Unsupported file extension.')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False,
+                              validators=[validate_file_extention])
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/article_cover/', null=False, blank=False,
+                             validators=[validate_file_extention])
+    content = RichTextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    meta_description = models.CharField(default='', max_length=512, null=False, blank=False)
+    meta_keywords = models.CharField(default='', max_length=512, null=False, blank=False)
+    promote = models.BooleanField(default=False)
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False,
+                             validators=[validate_file_extention])
+    # parent_id = models.ForeignKey('Category')
--- weblog/settings.py
+++ weblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'app_blog',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+# CKeditor
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "/uploads/"
+
+# Media Settings
+MEDIA_URL = '/files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
